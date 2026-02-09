0c7bdf923abd1c9440a9f642eefa4965dd22c29d
Cyril Pascal Becot <cyril.becot@cern.ch>
2026-02-09 10:49:20 +0100

    Add taxref url in init file

--- biolit/__init__.py
+++ biolit/__init__.py
@@ -7,6 +7,9 @@ DATADIR = ROOTDIR / "data"
 RAWDIR = DATADIR / "raw"
 EXPORTDIR = DATADIR / "exports"
 
+# /!\ URL emporaire de l'archive le temps que le museum corrige sa cybersec...
+TAXREFURL = "https://assets.patrinat.fr/files/referentiel/TAXREF_v18_2025.zip"
+
 
 mpl.rcParams["axes.spines.right"] = False
 mpl.rcParams["axes.spines.top"] = False
